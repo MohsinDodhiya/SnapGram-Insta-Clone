ea30cea647fa2fc357fc5e6c728b1e5b44a70a7d
MohsinDodhiya <mdodhiya653@rku.ac.in>
2024-11-12 22:08:32 +0530

    add create post

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,5 +1,5 @@
-import { INewUser } from "@/types";
-import { account, appwriteConfig, avatars, database } from "./config";
+import { INewPost, INewUser } from "@/types";
+import { account, appwriteConfig, avatars, database, storage } from "./config";
 import { ID, Query } from "appwrite";
 
 // ============================================================
@@ -30,7 +30,7 @@ export async function createUserAccount(user: INewUser) {
     return newUser;
   } catch (error) {
     console.log("Appwrite :: createNewUser ::  error : ", error);
-    return error;
+    return null;
   }
 }
 
@@ -52,7 +52,7 @@ export async function saveUserToDb(user: {
     return newUser;
   } catch (error) {
     console.log("Appwrite :: saveUserToDb ::  error : ", error);
-    return error;
+    return null;
   }
 }
 
@@ -97,6 +97,102 @@ export async function signOutAccount() {
     return session;
   } catch (error) {
     console.log("Appwrite :: signOutAccount ::  error : ", error);
+    return null;
+  }
+}
+
+// ============================================================
+// POSTS
+// ============================================================
+
+// ============================== CREATE POST
+export async function createPost(post: INewPost) {
+  try {
+    // upload img to storage
+    const uploadedFile = await uploadFile(post.file[0]);
+
+    if (!uploadedFile) throw Error;
+
+    // get file URL
+    const fileUrl = await getFilePreview(uploadedFile.$id);
+
+    if (!fileUrl) {
+      await deleteFile(uploadedFile.$id);
+      throw Error;
+    }
+
+    // Convert tags into array
+    const tags = post.tags?.replace(/ /g, "").split(",") || [];
+
+    //// Create post
+    const newPost = await database.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.postsCollectionId,
+      ID.unique(),
+      {
+        creater: post.userId,
+        caption: post.caption,
+        imageUrl: fileUrl,
+        imageId: uploadedFile.$id,
+        location: post.location,
+        tags: tags,
+      }
+    );
+    if (!newPost) {
+      await deleteFile(uploadedFile.$id);
+      throw Error;
+    }
+
+    return newPost;
+  } catch (error) {
+    console.log("Appwrite :: createPost ::  error : ", error);
+    return null;
+  }
+}
+
+// ============================== UPLOAD FILE
+export async function uploadFile(file: File) {
+  try {
+    const uploadedFile = await storage.createFile(
+      appwriteConfig.storageId,
+      ID.unique(),
+      file
+    );
+    return uploadedFile;
+  } catch (error) {
+    console.log("Appwrite :: uploadFile ::  error : ", error);
+    return null;
+  }
+}
+
+// ============================== FILE PREVIEW
+export async function getFilePreview(fileid: string) {
+  try {
+    const fileUrl = await storage.getFilePreview(
+      appwriteConfig.storageId,
+      fileid,
+      2000,
+      2000,
+      "top",
+      100
+    );
+    return fileUrl;
+  } catch (error) {
+    console.log("Appwrite :: getFilePreview ::  error : ", error);
+    return null;
+  }
+}
+
+// ============================== DELETE FILE
+export async function deleteFile(fileid: string) {
+  try {
+    const deleteFile = await storage.deleteFile(
+      appwriteConfig.storageId,
+      fileid
+    );
+    return deleteFile;
+  } catch (error) {
+    console.log("Appwrite :: deleteFile ::  error : ", error);
     return null;
   }
 }
