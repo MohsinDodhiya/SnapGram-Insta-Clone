687e96b3e3effdbf9a9978845567d6cdab1c2797
MohsinDodhiya <mdodhiya653@rku.ac.in>
2024-11-16 23:13:30 +0530

    add all user page

--- src/_root/pages/AllUsers.tsx
+++ src/_root/pages/AllUsers.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import Loader from "@/components/shared/Loader";
+import UserCard from "@/components/shared/UserCard";
+import { useToast } from "@/hooks/use-toast";
+import { useGetUsers } from "@/lib/react-query/queryesAndMutations";
 
 const AllUsers = () => {
-  return (
-    <div>AllUsers</div>
-  )
-}
+  const { toast } = useToast();
+  const { data: creaters, isLoading, isError: isErrorCreators } = useGetUsers();
 
-export default AllUsers
+  if (isErrorCreators) {
+    toast({
+      title: "Something went Wrong.",
+    });
+  }
+  return (
+    <div className="common-container">
+      <div className="user-container">
+        <h2 className="h3-bold md:h2-bold text-left w-full">All Users</h2>
+        {isLoading && !creaters ? (
+          <Loader />
+        ) : (
+          <ul className="user-grid">
+            {creaters?.documents.map((creater) => (
+              <li key={creater?.$id} className="flex-1 min-w-[200px] w-full  ">
+                <UserCard user={creater} />
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AllUsers;
