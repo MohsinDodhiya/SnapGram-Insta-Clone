dbc877211142778aee616ea9b4f2758bdf509e05
MohsinDodhiya <mdodhiya653@rku.ac.in>
2024-11-28 16:46:12 +0530

    vite issue

--- vite.config.ts
+++ vite.config.ts
@@ -9,9 +9,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
-  },
-  base: "/", // Ensure this matches your deployment base
 });
